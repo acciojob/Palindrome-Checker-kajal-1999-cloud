075bd3ca1853bc57d70af2486f47067ab61b0a57
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-31 00:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+ const cleanString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+  // Step 2: Compare with its reverse
+  const reversedString = cleanString.split('').reverse().join('');
+
+  // Step 3: Check if both strings are the same
+  return cleanString === reversedString;
 }
 module.exports = palindrome
